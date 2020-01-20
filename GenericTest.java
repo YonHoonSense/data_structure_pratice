211ef121d59b9b488bb4e6469602fcd1ffcf4b72
YonHoonSense <yh0520@naver.com>
2020-01-20 15:46:17 +0900

    Generic 예제

---
+++ GenericTest.java
@@ -0,0 +1,24 @@
+class GenericTest { 
+
+    static class GenericClass<T> {
+        private T xyz; // 자료형 T 변수 xyz
+        
+        GenericClass(T t){
+            this.xyz = t;
+        }
+
+        T getXyz() {
+            return xyz;
+        }
+
+    }
+
+    public static void main(String[] args){
+
+        GenericClass<String> s = new GenericClass<String>("ABC");
+        GenericClass<Integer> n = new GenericClass<Integer>(15);
+
+        System.out.println(s.getXyz());
+        System.out.println(n.getXyz());
+    }
+}
